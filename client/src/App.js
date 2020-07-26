810eed5f6a744f67d80688d0decc8ed0696dd2ab
Gerard Taylor <gerardt@twitter.com>
2020-07-26 09:45:28 -0400

    Client Side Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Grid, TextField } from '@material-ui/core';
+import YouTube from 'react-youtube';
+
+function App() {
+
+  const [videoLink, setVideoLink] = useState("");
+  const [videoTimestamp, setVideoTimestamp] = useState(0);
+
+  const onChange = (e) => {
+    setVideoLink(e.target.value)
+    console.log(e.target.value);
+  }
+
+  const getVideoId = () => {
+    if(videoLink === "" || videoLink === undefined) return "";
+    // www.youtube.com/watch?v=ID&...
+    let splitVideoLink = videoLink.split("v=")[1]
+    let ampersandLocation = splitVideoLink.indexOf("&");
+    if(ampersandLocation !== -1) {
+      return splitVideoLink.substring(0, ampersandLocation);
+    }
+    return splitVideoLink;
+  }
+
+  return (
+    <Grid container direction='column' justify='center' alignItems='center'>
+      <Grid item xs={12}>
+        <TextField 
+           value={videoLink}
+           name='videoLink'
+           placeholder='Enter a YouTube URL'
+           variant='outlined'
+           onChange={(e) => onChange(e)}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <YouTube
+          videoId={getVideoId()}
+          opts={{
+            width: '100%',
+            playerVars: {
+              start: parseInt(videoTimestamp)
+            }
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default App;
